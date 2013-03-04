e7fa34f1bb235fdfb43936af8c9e5586475d690f
isms <isaac.slavitt@gmail.com>
2013-03-03 21:41:00 -0500

    refactor intermediate functions out, code cleanup

--- README.md
+++ README.md
@@ -1,28 +1,42 @@
 # votebuilder-xls2json
 
-Takes an `xls` file exported from certain sections of Votebuilder and outputs well-formed JSON.
+Takes an `xls` file exported from certain sections of Votebuilder (a product of NGP VAN, Inc.) and outputs well-formed JSON.
 
 Usage: `python parse.py <exported xls>`
 
 Not all Votebuilder exports are set up the same way. This file works for exported files (such as Events List, Counts and Crosstabs, etc) which are actually an HTML fragment of the following form:
 
-```
-            <div>
-              <table class="ResultsGridViewControlStyle" [...] >
-                <tr class="ResultsHeaderStyle">
-                  <th scope="col"> [...] </th>
-                  [...]
-                </tr>
-            /   <tr class="ResultsRowStyle">
-            |     <td> [...] </td>
-            |     [...]
-(repeating) |   </tr>
-            |   <tr class="ResultsAlternateRowStyle"> [...]
-            |     <td> [...] </td>
-            |     [...]
-            \   </tr>
-              </table>
-            </div>
+```html
+<div>
+  <table class="ResultsGridViewControlStyle" [...] >
+    <tr class="ResultsHeaderStyle">
+      <th scope="col"> [...] </th>
+      [...]
+    </tr>
+    <tr class="ResultsRowStyle">
+      <td> [...] </td>
+      [...]
+    </tr>
+    <tr class="ResultsAlternateRowStyle"> [...]
+      <td> [...] </td>
+      [...]
+    </tr>
+  </table>
+</div>
 ```
 
-Votebuilder is a product of NGP VAN, Inc.
+It is also possible to use the intermediate functions within the module. For example:
+
+```python
+from parse import read_html_from_file, get_dicts_from_html
+
+# read an exported events list
+html = read_html_from_file('Report.xls')
+dicts = get_dicts_from_html(html)
+dicts[0]
+```
+
+Which would print a result like this:
+```python
+{'Scheduled': '6', 'Confirmed': '0', 'Completed': '0', 'Tentative': '0', 'Invited': '0', 'Walk In': '0', 'Conf Twice': '0', 'Paid': '0', 'ID': '55555', 'Wait List': '0', 'Cancelled': '0', 'Left Msg': '0', 'Time': '10:00 AM - 4:00 PM', 'Date': '1/1/13', 'No Show': '0', 'Type': 'Canvassing', 'Event': 'Anytown Canvass', 'Declined': '0'}
+```
--- parse.py
+++ parse.py
@@ -6,38 +6,52 @@ import sys
 import simplejson
 from lxml.html import fromstring
 
-ROW_CLASS_NAMES = ['ResultsHeaderStyle', 'ResultsRowStyle',
+TR_CLASS_NAMES = ['ResultsHeaderStyle', 'ResultsRowStyle',
     'ResultsAlternateRowStyle']
 
-def get_lines_from_html(html):
+def get_lists_from_html(html):
     """ Parse the file to find the headers and all content rows of the
-        table.
-
-        Returns a tuple of (headers, all other lines)
-    """
+        table. Returns a tuple of (headers, all other rows) """
     doc = fromstring(html)
-    # get all the table rows, headers should always be first
-    rows = []
-    for name in ROW_CLASS_NAMES:
-        rows += doc.find_class(name)
-    lines = [[child.text_content() for child in row.getchildren()]
-        for row in rows]
-    return (lines[0], lines[1:]) # (headers, all other lines)
+    # get all the tr elements, headers will always be first
+    trs = []
+    for class_name in TR_CLASS_NAMES:
+        trs += doc.find_class(class_name)
+    # extract the contents of all of the td (or th, for headers) children
+    rows = [[td.text_content() for td in tr.getchildren()] for tr in trs]
+    return (rows[0], rows[1:]) # (headers, all other rows)
 
-def get_row_dicts(headers, lines):
-    """ Take all lines as lists read in from the file and put the
-        row fields in the right order and sort lines by date """
-    return [dict((headers[i], line[i]) for i in range(len(headers)))
-        for line in lines]
+def get_dicts_from_lists(headers, contents):
+    """ Take all rows as lists turn them into dictionaries with the headers
+        as keys for all dicts and the values in each row as values in each
+        corresponding dictionary """
+    size = range(len(headers))
+    return [dict((headers[i], row[i]) for i in size) for row in contents]
+
+def get_dicts_from_html(html):
+    """ Higher level function to go straight from HTML to list of row
+        dictionaries by combining other functions """
+    headers, contents = get_lists_from_html(html)
+    return get_dicts_from_lists(headers, contents)
+
+def get_json_from_dicts(dicts):
+    """ Given list of row dictionaries, return pretty printed JSON """
+    return simplejson.dumps(dicts, sort_keys=True, indent=4)
+
+def get_json_from_html(html):
+    """ Higher level function to go straight from HTML to JSON by combining
+        other functions """
+    dicts = get_dicts_from_html(html)
+    return get_json_from_dicts(dicts)
+
+def read_html_from_file(filename):
+    """ Opens the file and returns the contents """
+    return open(filename, 'r').read()
 
 def main(filename):
     """ Open the desired file, parse html, and output json """
-    xls_file = open(filename, 'r')
-    html = xls_file.read()
-    xls_file.close()
-    headers, lines = get_lines_from_html(html)
-    rows = get_row_dicts(headers, lines)
-    json = simplejson.dumps(rows, sort_keys=True, indent=4)
+    html = read_html_from_file(filename)
+    json = get_json_from_html(html)
     print(json)
 
 if (__name__ == '__main__'):
